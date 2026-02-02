49620e438df930a09feef2f570ceca69a6adb8a1
Lia <aliyahrenomeron7@gmail.com>
2026-02-02 11:16:21 -0500

    created placeholder script for menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
-window.addEventListener("load", function() {
+document.addEventListener("DOMContentLoaded", () => {
     // NOTHING HERE YET BOZO
+    buttonWorks();
 })
+
+function buttonWorks() {
+    let start = document.getElementById("start-btn");
+
+    start.addEventListener("click", mouseClick);
+    
+    function mouseClick() {
+        console.log("gum.mp3")
+    }
+}
